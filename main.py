812d61f6e5a3321c8728ca5cbfbab6c58f85ea0d
recker624 <koolcephoid@gmail.com>
2026-01-05 21:35:42 +0530

    Fetched mnist dataset from sklearn and updated requirements.txt

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print("Hello world")
+from sklearn.datasets import fetch_openml
+import numpy as np
+
+# as_frames=False gives us pure numpy array
+mnist = fetch_openml('mnist_784', version=1 , as_frame=False)
+
+X_raw = mnist.data
+Y_raw = mnist.target
+
+print(f'Shape of actual data : {X_raw.shape}')
+print(f'Shape of the target values : {Y_raw.shape}')
