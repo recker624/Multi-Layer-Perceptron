84375cef22e565c2ae69b161a5412cce9ce4fee0
recker624 <koolcephoid@gmail.com>
2026-02-13 08:37:36 +0530

    Imports MNIST dataset for further data analysis

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print("Hello world")
+# import the actual dataset
+from sklearn.datasets import fetch_openml
+import numpy as np
+
+mnist = fetch_openml('mnist_784', version=1, as_frame=False)
+X_raw = mnist.data
+Y_raw = mnist.target
+
+
